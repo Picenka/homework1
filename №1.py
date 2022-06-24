71348ef6bf268bb853a8b7ff4555bfd5c6627634
den16 <deni.shl@yandex.ru>
2022-06-24 13:34:12 +0300

    1 go

---
+++ №1.py
@@ -0,0 +1,14 @@
+my_string = ['http://www.google.co.jp','http://google.com','http://google.co.jp','www.xakep.ru','https://youtube.com','https://university.ylab.site/python/lecture-1-hw/']
+
+
+def domain_name(url):
+    if '//' in url:
+        url=url.split('/')[2]
+    elif 'www' or 'web' in url:
+        return url.split('.')[1]
+    if url.count('.')>=2:
+        return url.split('.')[-3]
+    return url.split('.')[-2]
+
+for i in my_string:
+    print(i,' = ',domain_name(i))
